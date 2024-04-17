e1d158a064ccf9eca74b91f5183efbf77cb5cbec
Emmymike <163767882+Emmymike@users.noreply.github.com>
2024-04-17 11:08:04 +0100

    Add files via upload

---
+++ NETFLIX CLONE/index.js
@@ -0,0 +1,48 @@
+/*let x=5+7;
+
+
+console.log(x);
+let y=10+5*2
+console.log(y);
+const k=10/4*3-2;
+console.log(k)
+var g=30/10*5
+console.log(g)
+let x = 5 + 7;
+x= 20+ 3;
+console.log(x)
+const x = 5+7;
+x = 20+3;
+console.log(x)
+var x = 5+7;
+var x = 20+3;
+console.log(x)
+let hour=4
+if (hour>=10){
+    hour="Good morning"
+}
+else if (hour==5){
+    hour="Good evening"
+}
+else{
+    hour="Good night"
+}
+console.log(hour);*/
+let score = 55
+if (score > 70){
+    score ="A"
+}
+else if
+(score > 60) {
+    score = "B"
+}
+ else if (score > 50) { 
+  score = "C"
+}
+ else if(score > 40) {
+    score = "D"
+}
+else {
+    score = "F"
+}
+console.log(score)
---
+++ NETFLIX CLONE/yam.js
@@ -0,0 +1,31 @@
+/*let x= myfunction(10,5)
+function myfunction(a,b){
+    return a*b
+}
+console.log(x)
+let value= toCelcius(30)
+function toCelcius(fahrenheit){
+    return (5/9)*(fahrenheit-32)
+}
+console.log(value)
+let man= areaofcircle(15)
+function areaofcircle(radius){
+    return 22/7*radius*radius
+}
+console.log(man)
+for (x=2; x <= 4; x++){
+    console.log ("value of x is" + x);
+}
+for(let i=0; i<5; i++){
+    console.log(" i is given as:" + i)
+}
+ const person = {Fname: "james", Lname: "peter", age=20};
+ let text = "";
+ for (let x in person){
+text + person
+ }*/
+ function emma (x,y,z){
+    return a+b
+ }
+ let sum=emma (5+6);
+ console.log(sum)
